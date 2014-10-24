f8384a71ce40d4b211970fb23673377c667537c2
Tim Withers <timwithers@gmail.com>
2014-10-23 22:32:49 -0700

    adds sample main to drive program

--- StringInspector/Program.cs
+++ StringInspector/Program.cs
@@ -10,6 +10,22 @@ namespace StringInspector
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            ArticleGatherer article = new ArticleGatherer();
+
+            Inspector inspect = null;
+            string next = String.Empty;
+
+            do
+            {
+                next = article.GetNextArticle();
+                inspect = new Inspector(next);
+                Console.WriteLine("Most used character: " + inspect.GetMostUsed());
+            }
+            while (next.Length > 0);
+
+            Console.ReadKey();
         }
     }
 }
