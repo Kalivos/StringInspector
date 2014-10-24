ba2fdd68f0b8ca59938939b95bffbec54dea9b0b
Tim Withers <timwithers@gmail.com>
2014-10-23 22:23:45 -0700

    Adds a simple interface/implementation to fetch article text.

---
+++ StringInspector/ArticleGatherer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInspector
+{
+    public class ArticleGatherer : IArticleRetrieval
+    {
+        List<string> articles = new List<string>() { "κόσμε", "aabbccdb" };
+
+        public string GetNextArticle()
+        {
+            string text = String.Empty;
+
+            if (articles.Count > 0)
+            {
+                text = articles[0];
+                articles.RemoveAt(0);
+            }
+
+            return text;
+        }
+    }
+}
---
+++ StringInspector/IArticleRetrieval.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInspector
+{
+    interface IArticleRetrieval
+    {
+        /// <summary>
+        /// Stub for fetching the next article to pass into the parser
+        /// </summary>
+        /// <returns>Text of an Article</returns>
+        string GetNextArticle();
+    }
+}
